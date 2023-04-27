b7cbaf80d7c71042fb067c10da0062ca874426c1
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-27 21:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let p = document.getElementById('status');
+let body = document.querySelector('body');
+
+let btn = document.getElementById('enterBtn');
+
+btn.addEventListener('click', (event) => {
+	let h1 = document.createElement('h1');
+	h1.innerHTML = "Entered Metaverse";
+	body.replaceChild(h1, p);
+});
